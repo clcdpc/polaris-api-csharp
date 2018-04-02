c130a548747e72e93305d4b285feca8c07b59d6d
mfields <mfields@clcohio.org>
2018-04-02 19:55:09 -0400

    fix for boolean general error

--- src/polaris-api-csharp/Helpers/PatronRegistrationHelper.cs
+++ src/polaris-api-csharp/Helpers/PatronRegistrationHelper.cs
@@ -24,19 +24,15 @@ namespace Clc.Polaris.Api.Helpers
             var doc = new XDocument();
             var root = new XElement("PatronRegistrationCreateData");
 
-            var t = _params.GetType();
-            foreach (PropertyInfo info in t.GetProperties())
+            foreach (var info in _params.GetType().GetProperties())
             {
                 var val = info.GetValue(_params, null);
-
-                if (val != null)
-                {
-                    root.Add(new XElement(info.Name, val != null ? val.GetType() == typeof(DateTime) ? ((DateTime?)val).Value.ToString("s") : val.ToString() : null));
-                }
+                if (val == null) continue;
+                
+                root.Add(new XElement(info.Name, val.GetType() == typeof(DateTime) ? ((DateTime)val).ToString("s") : val));                
             }
 
             doc.Add(root);
-
 
             return doc.ToString();
         }
