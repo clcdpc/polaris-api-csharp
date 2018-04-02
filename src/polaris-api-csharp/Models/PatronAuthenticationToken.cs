b4af79a8d907726b588f8e98060fa50e9ccff779
mfields <mfields@clcohio.org>
2018-04-02 19:58:14 -0400

    add class for new patron auth

---
+++ src/polaris-api-csharp/Models/PatronAuthenticationToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clc.Polaris.Api.Models
+{
+    public class PatronAuthenticationToken
+    {
+        public string Barcode { get; set; }
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
