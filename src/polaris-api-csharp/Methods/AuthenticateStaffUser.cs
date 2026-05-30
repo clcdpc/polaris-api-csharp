84cfab08565983e857d72e3d857dadb70aa9577d
mtfields <mfields@clcohio.org>
2026-05-30 14:19:02 -0400

    Fold staff auth into PAPI execution (#55)

--- src/polaris-api-csharp/Methods/AuthenticateStaffUser.cs
+++ src/polaris-api-csharp/Methods/AuthenticateStaffUser.cs
@@ -16,8 +16,12 @@ namespace Clc.Polaris.Api
     {
         public async Task<IRestResponse<ProtectedToken>> AuthenticateStaffUserAsync(PolarisUser staffUser, CancellationToken cancellationToken = default)
         {
-            var url = "/protected/v1/1033/100/1/authenticator/staff";
-            return await ExecuteStaffAuthenticationPostAsync<ProtectedToken>(url, body: staffUser, cancellationToken: cancellationToken).ConfigureAwait(false);
+            var request = new PapiRestRequest(HttpMethod.Post, "/protected/v1/1033/100/1/authenticator/staff")
+            {
+                Body = staffUser
+            };
+
+            return await ExecutePapiAsync<ProtectedToken>(request, cancellationToken, ProtectedTokenPreloadMode.Skip).ConfigureAwait(false);
         }
 
 
