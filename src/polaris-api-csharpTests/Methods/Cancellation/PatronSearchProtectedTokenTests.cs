57006449e1bcc332d0d4e3e52f858a225b72cd39
mtfields <mfields@clcohio.org>
2026-06-07 15:21:31 -0400

    Stabilize protected token cancellation test

--- src/polaris-api-csharpTests/Methods/Cancellation/PatronSearchProtectedTokenTests.cs
+++ src/polaris-api-csharpTests/Methods/Cancellation/PatronSearchProtectedTokenTests.cs
@@ -1,6 +1,7 @@
 using Clc.Polaris.Api;
 using Clc.Polaris.Api.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,19 +18,27 @@ namespace Clc.Polaris.Api.Tests
         {
             var handler = new ProtectedTokenHttpMessageHandler();
             var client = CreateProtectedClient(handler);
-            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(10));
+            using var cancellationTokenSource = new CancellationTokenSource();
+            var searchTask = client.PatronSearchAsync("name=Smith", cancellationToken: cancellationTokenSource.Token);
 
-            try
-            {
-                await client.PatronSearchAsync("name=Smith", cancellationToken: cancellationTokenSource.Token);
-                Assert.Fail("Expected cancellation to propagate.");
-            }
-            catch (OperationCanceledException)
-            {
-            }
+            await WaitForAuthenticationRequestAsync(handler);
+            cancellationTokenSource.Cancel();
+
+            await Assert.ThrowsExceptionAsync<OperationCanceledException>(async () => await searchTask.ConfigureAwait(false));
 
             Assert.AreEqual(1, handler.AuthenticationRequestCount);
             Assert.AreEqual(0, handler.NonAuthenticationRequestCount);
+        }
+
+        private static async Task WaitForAuthenticationRequestAsync(ProtectedTokenHttpMessageHandler handler)
+        {
+            using var timeout = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            while (handler.AuthenticationRequestCount == 0 && !timeout.IsCancellationRequested)
+            {
+                await Task.Delay(10, timeout.Token).ConfigureAwait(false);
+            }
+
+            Assert.AreEqual(1, handler.AuthenticationRequestCount);
         }
     }
 }
