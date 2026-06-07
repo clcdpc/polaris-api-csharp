2732380cadc9614ad21d1530539742eff46d6795
Michael Fields <mfields@clcohio.org>
2026-06-07 15:50:52 -0400

    adjust namespaces

--- src/polaris-api-csharpTests/Models/BibGetResultTests.cs
+++ src/polaris-api-csharpTests/Models/BibGetResultTests.cs
@@ -4,7 +4,7 @@ using Clc.Polaris.Api.Tests;
 using Clc.Polaris.Api.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Clc.Polaris.Api.Tests.Models
+namespace Clc.Polaris.Api.Tests
 {
     [TestClass]
     [UnitCategory]
--- src/polaris-api-csharpTests/Models/PapiRestRequestFactoryTests.cs
+++ src/polaris-api-csharpTests/Models/PapiRestRequestFactoryTests.cs
@@ -3,7 +3,7 @@ using Clc.Polaris.Api.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net.Http;
 
-namespace Clc.Polaris.Api.Tests.Models
+namespace Clc.Polaris.Api.Tests
 {
     [TestClass]
     [UnitCategory]
