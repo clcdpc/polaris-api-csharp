80b298dc7e13bbf67ea1904c1f5b2cadf0b64785
mtfields <mfields@clcohio.org>
2026-06-07 14:56:46 -0400

    Normalize model test namespaces

--- src/polaris-api-csharpTests/Models/PapiRestRequestConstructorTests.cs
+++ src/polaris-api-csharpTests/Models/PapiRestRequestConstructorTests.cs
@@ -1,11 +1,10 @@
 using Clc.Polaris.Api;
 using Clc.Polaris.Api.Models;
-using Clc.Polaris.Api.Tests;
 using Clc.Rest.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net.Http;
 
-namespace Clc.Polaris.Api.Tests.Models
+namespace Clc.Polaris.Api.Tests
 {
     [TestClass]
     [UnitCategory]
--- src/polaris-api-csharpTests/Models/PapiRestRequestPathClassificationTests.cs
+++ src/polaris-api-csharpTests/Models/PapiRestRequestPathClassificationTests.cs
@@ -1,9 +1,8 @@
 using Clc.Polaris.Api;
 using Clc.Polaris.Api.Models;
-using Clc.Polaris.Api.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Clc.Polaris.Api.Tests.Models
+namespace Clc.Polaris.Api.Tests
 {
     [TestClass]
     [UnitCategory]
