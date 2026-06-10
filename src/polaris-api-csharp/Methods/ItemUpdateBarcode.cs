8759b3f58aed153c6a8989f8e04f8673cdb289e8
Michael Fields <mfields@clcohio.org>
2026-06-10 10:28:31 -0400

    fix ItemUpdateBarcode and add tests

--- src/polaris-api-csharp/Methods/ItemUpdateBarcode.cs
+++ src/polaris-api-csharp/Methods/ItemUpdateBarcode.cs
@@ -1,10 +1,11 @@
+using Clc.Polaris.Api.Models;
+using Clc.Polaris.Api.Validation;
+using Clc.Rest;
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using Clc.Polaris.Api.Validation;
-using Clc.Rest;
-using Clc.Polaris.Api.Models;
 
 namespace Clc.Polaris.Api
 {
@@ -12,15 +13,38 @@ namespace Clc.Polaris.Api
     {
         public async Task<IRestResponse<PapiResponseCommon>> ItemUpdateBarcodeAsync(string newBarcode, int? itemRecordId = null, int? transactionBranchId = null, string oldBarcode = "", CancellationToken cancellationToken = default)
         {
-            var url = $"/protected/v1/1033/100/1/{ProtectedToken.Placeholder}/cataloging/items/{(itemRecordId.HasValue ? itemRecordId.Value.ToString() : oldBarcode)}/barcode";
-            var body = new ItemUpdateBarcodeData { ItemBarcode = newBarcode, TransactionBranchId = transactionBranchId ?? OrganizationId };
+            Require.Argument(newBarcode);
+
+            string itemIdentifier;
+            var isBarcodeLookup = false;
+
+            if (itemRecordId is int itemRecordIdentifier)
+            {
+                itemIdentifier = itemRecordIdentifier.ToString();
+            }
+            else
+            {
+                Require.Argument(oldBarcode);
+                itemIdentifier = WebUtility.UrlEncode(oldBarcode);
+                isBarcodeLookup = true;
+            }
+
+            var url = $"/protected/v1/1033/100/1/{ProtectedToken.Placeholder}/cataloging/items/{itemIdentifier}/barcode";
+            var body = new ItemUpdateBarcodeData
+            {
+                ItemBarcode = newBarcode,
+                TransactionBranchId = transactionBranchId ?? OrganizationId
+            };
+
             var request = PapiRestRequest.Put(url, body: body);
             request.QueryParameters.Add("wsid", transactionBranchId ?? WorkstationId);
-            if (!string.IsNullOrWhiteSpace(oldBarcode)) { request.QueryParameters.Add("isBarcode", 1); }
+
+            if (isBarcodeLookup)
+            {
+                request.QueryParameters.Add("isBarcode", 1);
+            }
+
             return await ExecutePapiAsync<PapiResponseCommon>(request, cancellationToken).ConfigureAwait(false);
         }
-
-
-
     }
 }
